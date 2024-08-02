1dfb9049355e00e200cb43d5b34e74344b8e84aa
Akage369 <xaviercasado369@gmail.com>
2024-08-02 21:56:54 +0200

    5:24 - 5:56 Tutorial

--- Kitchen/Assets/Scripts/ProgressBarUI.cs
+++ Kitchen/Assets/Scripts/ProgressBarUI.cs
@@ -5,19 +5,28 @@ using UnityEngine.UI;
 
 public class ProgressBarUI : MonoBehaviour
 {
-    [SerializeField] private CuttingCounter cuttingCounter;
+    [SerializeField] private GameObject hasPregressGameObject;
     [SerializeField] private Image barImage;
+
+    private IHasProgress hasProgress;
 
     private void Start()
     {
-        cuttingCounter.OnProgressChanged += CuttingCounter_OnProgressChanged;
+        hasProgress = hasPregressGameObject.GetComponent<IHasProgress>();
+
+        if(hasProgress == null)
+        {
+            Debug.LogError("Game Object " + hasPregressGameObject + " does not have a component that implements IHasProgress!");
+        }
+
+        hasProgress.OnProgressChanged += HasProgress_OnProgressChanged;
 
         barImage.fillAmount = 0f;
 
         Hide();
     }
 
-    private void CuttingCounter_OnProgressChanged(object sender, CuttingCounter.OnProgressChangedEventArgs e)
+    private void HasProgress_OnProgressChanged(object sender, IHasProgress.OnProgressChangedEventArgs e)
     {
         barImage.fillAmount = e.progressNormalized;
 
