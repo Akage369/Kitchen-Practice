072797670ee13ef2e2a2591a78e7dad08b6ad7c7
Akage369 <xaviercasado369@gmail.com>
2024-08-03 17:25:11 +0200

    5:56 - 6:30 Tutorial

--- Kitchen/Assets/Scripts/Counters/CuttingCounter.cs
+++ Kitchen/Assets/Scripts/Counters/CuttingCounter.cs
@@ -46,6 +46,14 @@ public class CuttingCounter : BaseCounter, IHasProgress
             if (player.HasKitchenObject())
             {
                 //Player is carrying something
+                if (player.GetKitchenObject().TryGetPlate(out PlateKitchenObject plateKitchenObject))
+                {
+                    //Player is carruing a Plate
+                    if (plateKitchenObject.TryAddIngredient(GetKitchenObject().GetKitchenObjectSO()))
+                    {
+                        GetKitchenObject().DestroySelf();
+                    }
+                }
             }
             else
             {
