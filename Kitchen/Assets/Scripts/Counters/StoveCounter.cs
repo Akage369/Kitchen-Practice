1434b26023ddedda3cc0003a9478ddeabbf674ff
Akage369 <xaviercasado369@gmail.com>
2024-08-14 22:33:54 +0200

    7:43 - 8:06 Tutorial

--- Kitchen/Assets/Scripts/Counters/StoveCounter.cs
+++ Kitchen/Assets/Scripts/Counters/StoveCounter.cs
@@ -6,7 +6,6 @@ using UnityEngine;
 public class StoveCounter : BaseCounter, IHasProgress
 {
     public event EventHandler<IHasProgress.OnProgressChangedEventArgs> OnProgressChanged;
-
     public event EventHandler<OnStateChangedEventArgs> OnStateChanged;
     public class OnStateChangedEventArgs : EventArgs
     {
--- Kitchen/Assets/Scripts/DeliveryManager.cs
+++ Kitchen/Assets/Scripts/DeliveryManager.cs
@@ -7,6 +7,8 @@ public class DeliveryManager : MonoBehaviour
 {
     public event EventHandler OnRecipeSpawned;
     public event EventHandler OnRecipeCompleted;
+    public event EventHandler OnRecipeSuccess;
+    public event EventHandler OnRecipeFailed;
     public static DeliveryManager Instance { get; private set; }
 
     [SerializeField] private RecipeListSO recipeListSO;
@@ -82,6 +84,7 @@ public class DeliveryManager : MonoBehaviour
                     waitingRecipeSOList.RemoveAt(i);
 
                     OnRecipeCompleted?.Invoke(this, EventArgs.Empty);
+                    OnRecipeSuccess?.Invoke(this, EventArgs.Empty);
 
                     return;
                 }
@@ -90,6 +93,7 @@ public class DeliveryManager : MonoBehaviour
 
         //No matches found!
         //Player did not deliver a correct recipe
+        OnRecipeFailed?.Invoke(this, EventArgs.Empty);
     }
 
     public List<RecipeSO> GetWaitingRecipeSOList()
