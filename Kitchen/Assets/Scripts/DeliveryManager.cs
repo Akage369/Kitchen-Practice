7e6c95fa3510ba48c62c0cbc4d1c6513fad71e20
Akage369 <xaviercasado369@gmail.com>
2024-08-16 17:16:56 +0200

    8:06 - 8:49 Tutorial

--- Kitchen/Assets/Scripts/DeliveryManager.cs
+++ Kitchen/Assets/Scripts/DeliveryManager.cs
@@ -18,6 +18,7 @@ public class DeliveryManager : MonoBehaviour
     private float spawnRecipeTimerMax = 4f;
 
     private int waitingRecipesMax = 4;
+    private int successfulRecipesAmount;
 
     private void Awake()
     {
@@ -81,6 +82,8 @@ public class DeliveryManager : MonoBehaviour
                 {
                     //Player delivered the correct recipe!
 
+                    successfulRecipesAmount++;
+
                     waitingRecipeSOList.RemoveAt(i);
 
                     OnRecipeCompleted?.Invoke(this, EventArgs.Empty);
@@ -99,5 +102,10 @@ public class DeliveryManager : MonoBehaviour
     public List<RecipeSO> GetWaitingRecipeSOList()
     {
         return waitingRecipeSOList; 
+    }
+
+    public int GetSuccessfulRecipesAmount()
+    {
+        return successfulRecipesAmount;
     }
 }
